7666204b435cdb949876f3c06b237d8c5ee3f2da
daffa03x <mobadaff@gmail.com>
2022-06-12 07:46:35 +0700

    ok

---
+++ src/Koneksi/GlobalVar.java
@@ -0,0 +1,46 @@
+package Koneksi;
+import java.sql.*;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Nurul Kafi
+ */
+public class GlobalVar {
+    public static String nama;
+    public static String namabarang;
+    public static String harga;
+    public static String kode;
+    public static String id_user;
+    public static String no_pen;
+    public static String hak_akses;
+    public static String KodePoli;
+    public static String NamaPoli;
+    public static String id_jenis;
+    public static String namajenis;
+    public static String tarif;
+    public static String KodeJadwal;
+    public static String KodeObat;
+    public static String NamaObat;
+    public static String Merk;
+    public static String Stock;
+    public static String Harga;
+    public static String idbiaya;
+    public static String NoPendaftaran;
+    public static String NoPemeriksaan;
+    public static String nopemrikprint;
+    public static String nopendafprint;
+    public static String NoPen;
+    public static String Status;
+    public static int JmlData;
+    public static int TotalObat;
+    public static int TotalJenisBiaya;
+//    public static String tanggalAwal;
+//    public static String tanggalAkhir;
+//    
+ 
+}
